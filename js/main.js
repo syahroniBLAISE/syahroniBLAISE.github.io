327a4a5fc8f906dbcbcdd8931d440ea34df47a2c
syahroniBLAISE <syahronigantolle93@gmail.com>
2021-10-12 23:33:51 +0700

    penambahan tombol chat direct to Whatsapp

--- js/main.js
+++ js/main.js
@@ -89,6 +89,19 @@ function main() {
 
 }());
 
+$(function () {
+  $('.floating-wpp').floatingWhatsApp({
+      phone: '99999999999',
+      popupMessage: 'Welcome to iamrohit.in',
+      showPopup: true,
+      position: 'right', // left or right
+      autoOpen: false, // true or false
+      //autoOpenTimer: 4000,
+      message: 'Hello how are you my name is rohit',
+      //headerColor: 'orange', // enable to change msg box color
+      headerTitle: 'Whatsapp Message Box',
+  });
+});
 
 }
 main();
