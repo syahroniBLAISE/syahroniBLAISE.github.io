cda541b421858db03c00ef670b7a56b907ad7488
syahroniBLAISE <syahronigantolle93@gmail.com>
2022-06-01 17:26:22 +0700

    perubahan nomor WA

---
+++ js/whatsapp.js
@@ -0,0 +1,11 @@
+$("#myDiv").floatingWhatsApp({
+  phone: "+6281277546236",
+  popupMessage: "Ada yang bisa di bantu bosku ..?",
+  message: "saya ingin sablon baju",
+  showPopup: true,
+  showOnIE: false,
+  headerTitle: "Welcome!",
+  headerColor: "green",
+  backgroundColor: "black",
+  buttonImage: '<img src="floating-whatsapp-master/whatsapp.svg" />',
+});
